36ea43f4dc70b273ad868f8c75f551f741e043c9
Robert Barry <rb4080@gmail.com>
2016-02-19 14:57:41 -0500

    Add show and hide keyboard functionality

--- On The Map/LoginViewController.swift
+++ On The Map/LoginViewController.swift
@@ -16,7 +16,10 @@ class LoginViewController: UIViewController {
     // OUTLETS
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-
+    
+    
+    
+    // IOS OVERRIDES
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,6 +32,50 @@ class LoginViewController: UIViewController {
         
         passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password",
             attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()])
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        subscribeToKeyboardNotifications()
+    }
+    
+    
+    
+    // HELPER FUNCTIONS
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+        
+    }
+    
+    // Move the keyboard to show the bottom text field
+    func keyboardShowAndHide(notification: NSNotification) {
+        
+        // If the notification is WillShow, move the view up
+        if notification.name == UIKeyboardWillShowNotification {
+            view.frame.origin.y -= getKeyboardHeight(notification) - 150
+        } else { // else move the view down
+            view.frame.origin.y = 0
+        }
+        
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardShowAndHide:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardShowAndHide:", name: UIKeyboardWillHideNotification, object: nil)
+        
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+        
     }
 
 }
