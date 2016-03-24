f04de9072c9376c333b0f02d1bf7646a53103c9d
Robert Barry <rb4080@gmail.com>
2016-03-24 15:19:00 -0400

    Add constants struct.

---
+++ On The Map/UdacityConstants.swift
@@ -0,0 +1,16 @@
+//
+//  UdacityConstants.swift
+//  On The Map
+//
+//  Created by Robert Barry on 3/24/16.
+//  Copyright © 2016 Robert Barry. All rights reserved.
+//
+
+import Foundation
+
+struct UdacityConstants {
+    
+    static let AppId = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+    static let ApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+    
+}
